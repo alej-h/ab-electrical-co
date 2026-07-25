2792ee23d658e02b9b19bcccbb21872562b464af
alej-h <alejandroh321@outlook.com>
2026-07-24 19:25:33 -0700

    Add Lake Havasu, fix javascript

--- site.js
+++ site.js
@@ -56,18 +56,21 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 // Gallery Click to Advance
-const viewer = document.querySelector('.viewer');
-const imgs = viewer.querySelectorAll('img');
-const dots = document.querySelectorAll('.dot');
-let current = 0;
+const allViewers = document.querySelectorAll('.viewer');
 
-viewer.addEventListener('click', function() {
-  imgs[current].classList.remove('active');
-  dots[current].classList.remove('active');
-  current++;
-  if (current === imgs.length) {
-    current = 0;
-  }
-  imgs[current].classList.add('active');
-  dots[current].classList.add('active');
+allViewers.forEach(function(viewer) {
+  const imgs = viewer.querySelectorAll('img');
+  const dots = viewer.parentElement.querySelectorAll('.dot');
+  let current = 0;
+
+  viewer.addEventListener('click', function() {
+    imgs[current].classList.remove('active');
+    dots[current].classList.remove('active');
+    current++;
+    if (current === imgs.length) {
+      current = 0;
+    }
+    imgs[current].classList.add('active');
+    dots[current].classList.add('active');
+  });
 });
